187afbbebd137e26cd55576150a4d087ccfa4023
Ludovik Coba <ludovancoba@gmail.com>
2020-08-17 15:31:35 +0200

    tree serializer

---
+++ 08_2020/tree_serialize_deserialize.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def serialize(root):
+    if root == None:
+        return "/"
+    append_marker = "+"
+
+    s = str(root.val)
+
+    return s + append_marker + serialize(root.left) + append_marker + serialize(root.right)        
+
+def deserialize(txt):
+
+    values = iter(txt.split("+"))
+
+    def deserializer():
+        val = next(values)
+
+        if val == "/":
+            return None
+        else:
+            node = Node(val)
+            node.left = deserializer()
+            node.right = deserializer()
+            return node
+
+    return deserializer()
+
+
+
+
+if __name__ == "__main__":
+    node = Node('root', Node('left', Node('left.left')), Node('right'))
+
+    print (serialize(node))
+    print (deserialize(serialize(node)).left.left.val)
+    #assert deserialize(serialize(node)).left.left.val == 'left.left'
