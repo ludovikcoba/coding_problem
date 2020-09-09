28d8e46573412319d1291a5b65193f57a6d5927f
Ludovik Coba <ludovancoba@gmail.com>
2020-09-09 19:45:02 +0200

    o(1)

--- 08_2020/prob_9_non_adjacent_sum.py
+++ 08_2020/prob_9_non_adjacent_sum.py
@@ -7,17 +7,24 @@ Follow-up: Can you do this in O(N) time and constant space?
 """
 
 def non_adjacent_sum(a:list):
-    sums = [0] * len(a) #can be imporved to having only two variables
+
+    if len(a) == 0:
+        return 0
+    
+    if len(a) == 1:
+        return a[0]
+
+    sums = [0] * 2 
 
     sums[0] = a[0] 
     sums[1] = max(a[0], a[1])
 
     for i in range(2, len(a)):
-        temp = max(sums[i-2], sums[i-2] + a[i], a[i])
-        
-        sums[i] = max(sums[i-1], temp)
+        temp = max(sums[i%2], sums[i%2] + a[i]) # the number we are adding is negative so we might not want it
+        temp = max(temp, a[i]) # we had only negative numbers so far so we might not need them
+        sums[i%2] = max(sums[(i-1)%2], temp) # we compare it to the first
 
-    return sums[len(a) - 1]
+    return sums[(len(a) - 1)%2]
 
 
 if __name__ == "__main__":
